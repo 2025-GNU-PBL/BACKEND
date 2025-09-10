8d514b09d17fc6adbd15f19c04d3febf89673ff1
HongJongMin <gwa7417@gmail.com>
2025-09-10 13:04:43 +0900

    feat: 기초셋팅

---
+++ src/test/java/gnu/project/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gnu.project.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
