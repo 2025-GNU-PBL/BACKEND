e06c5fea10988067ea8b74eb32170af62b7a617d
HongJongMin <gwa7417@gmail.com>
2025-09-22 14:02:10 +0900

    feat: Response 변경

--- src/main/java/gnu/project/backend/owner/dto/response/OwnerResponse.java
+++ src/main/java/gnu/project/backend/owner/dto/response/OwnerResponse.java
@@ -14,13 +14,11 @@ public record OwnerResponse(
     String bankAccount,
     UserRole userRole,
 
-    // OAuth 정보
     String email,
     String name,
     String socialId,
     SocialProvider socialProvider,
 
-    // BaseEntity 정보
     LocalDateTime createdAt,
     LocalDateTime updatedAt
 ) {
@@ -34,14 +32,10 @@ public record OwnerResponse(
             owner.getBzNumber(),
             owner.getBankAccount(),
             owner.getUserRole(),
-
-            // OAuth 정보 추출
             owner.getOauthInfo().getEmail(),
             owner.getOauthInfo().getName(),
             owner.getOauthInfo().getSocialId(),
             owner.getOauthInfo().getSocialProvider(),
-
-            // BaseEntity 정보 추출
             owner.getCreatedAt(),
             owner.getUpdatedAt()
         );
