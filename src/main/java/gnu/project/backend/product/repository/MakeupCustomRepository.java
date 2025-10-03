863afd46f2aa4f99c6f0693b843df8460dd8edea
HongJongMin <gwa7417@gmail.com>
2025-10-03 19:42:49 +0900

    feat: Pagination 구현

--- src/main/java/gnu/project/backend/product/repository/MakeupCustomRepository.java
+++ src/main/java/gnu/project/backend/product/repository/MakeupCustomRepository.java
@@ -1,10 +1,15 @@
 package gnu.project.backend.product.repository;
 
+import gnu.project.backend.product.dto.response.MakeupPageResponse;
 import gnu.project.backend.product.dto.response.MakeupResponse;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MakeupCustomRepository {
 
+
     MakeupResponse findByMakeupId(final Long id);
+
+    List<MakeupPageResponse> searchMakeup(final int pageSize, final int pageNumber);
 }
--- src/main/java/gnu/project/backend/product/repository/impl/MakeupRepositoryImpl.java
+++ src/main/java/gnu/project/backend/product/repository/impl/MakeupRepositoryImpl.java
@@ -3,10 +3,16 @@ package gnu.project.backend.product.repository.impl;
 import static gnu.project.backend.product.entity.QImage.image;
 import static gnu.project.backend.product.entity.QMakeup.makeup;
 
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import gnu.project.backend.product.dto.response.MakeupPageResponse;
 import gnu.project.backend.product.dto.response.MakeupResponse;
 import gnu.project.backend.product.entity.Makeup;
 import gnu.project.backend.product.repository.MakeupCustomRepository;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -15,6 +21,10 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class MakeupRepositoryImpl implements MakeupCustomRepository {
 
+    private static final OrderSpecifier<?>[] MAKEUP_DEFAULT_ORDER = {
+        makeup.createdAt.desc(),
+        makeup.id.desc()
+    };
     private final JPAQueryFactory query;
 
     @Override
@@ -26,5 +36,45 @@ public class MakeupRepositoryImpl implements MakeupCustomRepository {
             .where(makeup.id.eq(id))
             .fetchOne();
         return MakeupResponse.from(Objects.requireNonNull(result));
+    }
+
+    @Override
+    public List<MakeupPageResponse> searchMakeup(final int pageSize, final int pageNumber) {
+        return pagination(query
+                .select(createLessonResponse())
+                .from(makeup)
+                .leftJoin(makeup.images, image)
+                .where(image.displayOrder.eq(0))
+                .orderBy(MAKEUP_DEFAULT_ORDER),
+            pageSize,
+            pageNumber
+        ).fetch();
+    }
+
+    private ConstructorExpression<MakeupPageResponse> createLessonResponse() {
+        return Projections.constructor(
+            MakeupPageResponse.class,
+            makeup.id,
+            makeup.name,
+            makeup.style,
+            makeup.starCount,
+            makeup.address,
+            makeup.detail,
+            makeup.price,
+            makeup.type,
+            makeup.availableTimes,
+            makeup.createdAt,
+            image.url
+        );
+    }
+
+    private <T> JPAQuery<T> pagination(
+        final JPAQuery<T> query,
+        final Integer pageSize,
+        final Integer pageNumber
+    ) {
+        return query
+            .offset((long) (pageNumber - 1) * pageSize)
+            .limit(pageSize);
     }
 }
