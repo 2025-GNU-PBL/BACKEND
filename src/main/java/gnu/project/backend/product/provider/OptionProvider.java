9ab897db998e5b1160680c80066637d60f382139
HongJongMin <gwa7417@gmail.com>
2025-10-05 23:55:01 +0900

    feat: Provider 수정

--- src/main/java/gnu/project/backend/product/provider/OptionProvider.java
+++ src/main/java/gnu/project/backend/product/provider/OptionProvider.java
@@ -3,12 +3,17 @@ package gnu.project.backend.product.provider;
 import gnu.project.backend.product.dto.request.OptionCreateRequest;
 import gnu.project.backend.product.entity.Option;
 import gnu.project.backend.product.entity.Product;
+import gnu.project.backend.product.repository.OptionRepository;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OptionProvider {
+
+    private final OptionRepository optionRepository;
 
     public void createOptions(
         final Product product,
@@ -25,5 +30,6 @@ public class OptionProvider {
             );
         }
         product.addAllOption(optionList);
+        optionRepository.saveAll(optionList);
     }
 }
--- src/main/java/gnu/project/backend/product/provider/TagProvider.java
+++ src/main/java/gnu/project/backend/product/provider/TagProvider.java
@@ -3,12 +3,17 @@ package gnu.project.backend.product.provider;
 import gnu.project.backend.product.dto.request.TagRequest;
 import gnu.project.backend.product.entity.Product;
 import gnu.project.backend.product.entity.Tag;
+import gnu.project.backend.product.repository.TagRepository;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class TagProvider {
+
+    private final TagRepository tagRepository;
 
     public void createTag(
         final Product product,
@@ -19,5 +24,6 @@ public class TagProvider {
             tags.add(Tag.ofCreate(product, request.tagName()));
         }
         product.addAllTag(tags);
+        tagRepository.saveAll(tags);
     }
 }
