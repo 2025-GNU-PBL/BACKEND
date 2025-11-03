cb5fae82932c81c8fbeb662cd8bac33268c7e9c0
HongJongMin <gwa7417@gmail.com>
2025-11-03 16:47:44 +0900

    feat: @Transactional 추가

--- src/main/java/gnu/project/backend/product/service/DressService.java
+++ src/main/java/gnu/project/backend/product/service/DressService.java
@@ -27,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class DressService {
 
