bf46c5ef37674e63769c7f97cfff4847412ecebb
HongJongMin <gwa7417@gmail.com>
2025-11-05 10:44:40 +0900

    refactor: 태그 구현

---
+++ src/main/java/gnu/project/backend/product/enumerated/StudioTag.java
@@ -0,0 +1,14 @@
+package gnu.project.backend.product.enumerated;
+
+public enum StudioTag {
+    PORTRAIT_FOCUSED,
+    VARIED_BACKGROUND,
+    PORTRAIT_AND_BACKGROUND,
+    HANOK,
+    GARDEN,
+    NIGHT,
+    ROAD,
+    UNDERWATER,
+    PET_FRIENDLY
+
+}
---
+++ src/main/java/gnu/project/backend/product/enumerated/WeddingHallTag.java
@@ -0,0 +1,16 @@
+package gnu.project.backend.product.enumerated;
+
+public enum WeddingHallTag {
+    GENERAL,
+    CONVENTION,
+    HOTEL,
+    HOUSE,
+    RESTAURANT,
+    HANOK,
+    CHURCH,
+    CHAPEL,
+    SMALL,
+    OUTDOOR_GARDEN,
+    TRADITIONAL_WEDDING
+
+}
