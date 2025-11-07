88a05b429de9c0ef134ffb09d96ec338ef088224
khakionblack <74r2k@naver.com>
2025-11-07 11:35:50 +0900

    code refactiong

--- src/main/java/gnu/project/backend/cart/dto/request/CartAddRequest.java
+++ src/main/java/gnu/project/backend/cart/dto/request/CartAddRequest.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 public record CartAddRequest(
         @NotNull Long productId,
         LocalDateTime desireDate,
+        @NotNull
         @Min(1)Integer quantity
         // @Size(max = 500) String memo
 ) {
