460f3619a9a1e50c7efa884f66298326acc102fb
HongJongMin <gwa7417@gmail.com>
2025-11-15 17:26:48 +0900

    feat: 기능 fix 및 cart count api 구현

--- src/main/java/gnu/project/backend/cart/repository/CartRepository.java
+++ src/main/java/gnu/project/backend/cart/repository/CartRepository.java
@@ -1,10 +1,11 @@
 package gnu.project.backend.cart.repository;
 
 import gnu.project.backend.cart.entity.Cart;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
     Optional<Cart> findByCustomer_Id(Long customerId);
+
 }
