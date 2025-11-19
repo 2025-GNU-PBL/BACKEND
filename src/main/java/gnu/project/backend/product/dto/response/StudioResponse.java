396a3f51cbc2f21e0a38026df9c71f5e678ba312
HongJongMin <gwa7417@gmail.com>
2025-11-19 10:54:27 +0900

    refactor: Response 수정

--- src/main/java/gnu/project/backend/product/dto/response/StudioResponse.java
+++ src/main/java/gnu/project/backend/product/dto/response/StudioResponse.java
@@ -15,7 +15,6 @@ public record StudioResponse(
     Integer price,
     String availableTimes,
     Region region,
-
     List<ImageResponse> images,
     List<OptionResponse> options,
     List<TagResponse> tags
